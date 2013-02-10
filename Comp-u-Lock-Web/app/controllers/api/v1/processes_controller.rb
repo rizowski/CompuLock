bc4b931c67d67c4a13c7ed6d7b597ea0678d9a09
Colten Rouska <crouska@gmail.com>
2013-02-10 16:11:33 -0700

    [API] Processes Create has been created

--- Comp-u-Lock-Web/app/controllers/api/v1/processes_controller.rb
+++ Comp-u-Lock-Web/app/controllers/api/v1/processes_controller.rb
@@ -6,9 +6,39 @@ module Api
 
 		def create
 			token = params[:auth_token]
+			process = JSON.parse params[:process]
 			if token.nil?
 				render :status => 400,
 					:json => { :message => "The request must contain an auth token."}
+				return
+			end
+			account_id = process["account_id"]
+			if account_id.nil?
+				render :status => 400,
+					:json => { :message => "The request must contain an account id."}
+				return
+			end
+
+			if process["name"].nil?
+				render :status => 400,
+					:json => { :message => "The request must contain a name."}
+				return
+			end
+
+			@user = User.find_by_authentication_token token
+			@accounts = Account.where computer_id: @user.computer_ids
+
+			unless @accounts.pluck(:id).include? account_id
+				render :status => 401,
+					:json => { :message => "The request was declined. Check account_id."}
+				return
+			end
+			@process = AccountProcess.new(process)
+			if @process.save
+				render json: {process: @process}
+			else
+				render :status => 400,
+					:json => { :message => "There was a problem saving the entity."}
 				return
 			end
 		end
