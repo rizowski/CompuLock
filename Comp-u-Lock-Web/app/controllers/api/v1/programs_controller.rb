b5a7e7e46f1855ea8a94e2d4bc940b80bf0c0958
Colten Rouska <crouska@gmail.com>
2013-02-10 16:09:08 -0700

    [API] Program Create was created

--- Comp-u-Lock-Web/app/controllers/api/v1/programs_controller.rb
+++ Comp-u-Lock-Web/app/controllers/api/v1/programs_controller.rb
@@ -6,9 +6,39 @@ module Api
 
 		def create
 			token = params[:auth_token]
+			program = JSON.parse params[:program]
 			if token.nil?
 				render :status => 400,
 					:json => { :message => "The request must contain an auth token."}
+				return
+			end
+			account_id = program["account_id"]
+			if account_id.nil?
+				render :status => 400,
+					:json => { :message => "The request must contain an account id."}
+				return
+			end
+
+			if program["name"].nil?
+				render :status => 400,
+					:json => { :message => "The request must contain a name."}
+				return
+			end
+
+			@user = User.find_by_authentication_token token
+			@accounts = Account.where computer_id: @user.computer_ids
+
+			unless @accounts.pluck(:id).include? account_id
+				render :status => 401,
+					:json => { :message => "The request was declined. Check account_id."}
+				return
+			end
+			@program = AccountProgram.new(program)
+			if @program.save
+				render json: {program: @program}
+			else
+				render :status => 400,
+					:json => { :message => "There was a problem saving the entity."}
 				return
 			end
 		end
