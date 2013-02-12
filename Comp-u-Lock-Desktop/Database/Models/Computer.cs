de529abe899c2d8fafe8ea8e6ed9e0642c2c1bbf
Colten Rouska <crouska@gmail.com>
2013-02-11 21:27:59 -0700

    [DB] Account Model added

---
+++ Comp-u-Lock-Desktop/Database/Models/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    class Computer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Enviroment { get; set; }
+        public string IpAddress { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
