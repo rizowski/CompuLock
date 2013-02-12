3d98ae0f1f3583aab8992d271c636fc1481f35e3
Colten Rouska <crouska@gmail.com>
2013-02-11 21:32:13 -0700

    [DB] Account Added

---
+++ Comp-u-Lock-Desktop/Database/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Domain { get; set; }
+        public bool Tracking { get; set; }
+        public TimeSpan AllottedTime { get; set; }
+        public TimeSpan UsedTime { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public ICollection<History> Histories { get; set; }
+        public ICollection<Process> Processes { get; set; }
+        public ICollection<Program> Programs { get; set; }
+    }
+}
