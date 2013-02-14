a7f124090a26beca726f8de474e31243ebec055a
Colten Rouska <crouska@gmail.com>
2013-02-13 22:23:04 -0700

    Committing a successful build after crash

--- Comp-u-Lock-Desktop/Service/MainService.cs
+++ Comp-u-Lock-Desktop/Service/MainService.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Validation;
-using System.Linq;
-using System.Text;
-using CompuLockDesktop;
 using Database.Models;
 using Service.Db;
 using Service.Profile;
