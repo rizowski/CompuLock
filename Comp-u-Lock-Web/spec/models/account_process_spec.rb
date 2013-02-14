6f6d8cfb16f80b67050d3cfe02cf84cac607b9ba
Colten Rouska <crouska@gmail.com>
2013-02-14 16:05:48 -0700

    [WEB] Added Process test

--- Comp-u-Lock-Web/spec/models/account_process_spec.rb
+++ Comp-u-Lock-Web/spec/models/account_process_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe AccountProcess do
-  it "has a valid factory"
+  it "has a valid factory" do
+  	FactoryGirl.create(:account_process).should be_valid
+  end
+
 end
+ 
