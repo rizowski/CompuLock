95f9e623109be0dd14c32ab54521dc1defb810a8
Colten Rouska <crouska@gmail.com>
2013-02-15 23:14:38 -0700

    Added new classes for limiting classes

---
+++ Comp-u-Lock-Desktop/Database/Models/Day.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Day
+    {
+        List<Hour> Hours { get; set; }
+
+        public Day(List<Hour>  hours)
+        {
+            Hours = hours;
+        }
+
+        public Day()
+        {
+            
+        }
+    }
+
+    public class Hour
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public Hour(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Hour()
+        {
+            
+        }
+    }
+}
---
+++ Comp-u-Lock-Desktop/Database/Models/Restriction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Database.Models
+{
+    class Restriction
+    {
+        List<Day> Days { get; set; }
+        public int AccountId { get; set; }
+
+        public Restriction(List<Day> days)
+        {
+            Days = days;
+        }
+
+        public Restriction()
+        {
+        }
+    }
+}
